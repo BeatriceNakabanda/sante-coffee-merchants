71f87bbb589da073791d369a1692b8e858cfb171
EmmellaBeatrice <danahbeat@gmail.com>
2020-07-09 02:14:44 +0300

    Modified layouts

--- app/src/main/java/com/example/santecoffeemerhants/MainActivity.kt
+++ app/src/main/java/com/example/santecoffeemerhants/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.santecoffeemerhants
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ListView
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.santecoffeemerhants.data.Entity.RegionalManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity()  {
     private lateinit var regionalManager: RegionalManager
@@ -19,6 +27,21 @@ class MainActivity : AppCompatActivity()  {
 //        if (regionalManager != null){
 //            user.setText("Welcome " + regionalManager.name)
 //        }
+        val actionBar: ActionBar? = supportActionBar
+        if (actionBar != null) {
+            actionBar.setTitle("Farmers")
+        }
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, NewFarmerActivity::class.java)
+            startActivity(intent)
+        }
 
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        return true
     }
 }
